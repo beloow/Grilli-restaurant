1e929d159eda61bdc418b374fd72a5d2b4c90d5e
beloow <quevaqueva11@gmail.com>
2024-06-21 16:38:39 +0200

    Menu nav Js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Retrait du Préload une fois le site chargé
+////// Retrait du Préload une fois le site chargé //////
 
 const preloader = document.querySelector("[data-preaload]");
 
@@ -8,3 +8,42 @@ window.addEventListener("load", function () {
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 });
+
+////// addEventListener sur des elements multiples //////
+
+const addEventOnElements = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++){
+        elements[i].addEventListener(eventType, callback);
+    };
+};
+
+////// Navbar //////
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+////// header //////
+
+const header = document.querySelector("[data-header]");
+
+let lastScrollPos = 0;
+
+const hideHeader = function() {
+}
+
+window.addEventListener("scroll", function() {
+    if (window.screenY >= 50) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+})
